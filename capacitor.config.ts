68a63c6e4356e83a6b4a3124bbfedfc91357af4b
Gloria Kiawa <asglor@gmail.com>
2025-02-03 14:44:42 +0100

    dist to www

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.groupthree.styleswipe',
   appName: 'StyleSwipe',
-  webDir: 'dist'
+  webDir: 'www'
 };
 
 export default config;
